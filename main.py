e04f711c074504f3c51067fe6d132098c78a02d4
Redway67 <redway67@gmail.com>
2026-06-23 12:18:07 +0600

    3

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+from dotenv import load_dotenv
+
 from edu_assistant.config import Config
+from edu_assistant.llm_client import get_llm_client
 
-# Create config file path
+INPUT_PROMPT = "Почему началась Первая мировая война?"
+
+load_dotenv()
+
 config = Config.from_yaml_file("config.yml")
-print("Full config:", config)
-print("LLM model:", config.llms["api"].model)
+llm_config = config.llms["ollama"]
 
-system_prompt = config.render_system_instructions(role="history_tutor", template="tutor_quick_answer")
-print("System prompt:", system_prompt)
+system_prompt = config.render_system_instructions(
+    role="history_tutor",
+    template="tutor_quick_answer",
+)
+llm_client = get_llm_client(llm_config)
+
+response = llm_client.responses.create(
+    model=llm_config.model,
+    instructions=system_prompt,
+    input=INPUT_PROMPT,
+    max_output_tokens=llm_config.max_output_tokens,
+)
+
+print(response.output_text)
